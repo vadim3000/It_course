579d165b191222aaa53527c1fbd5939a8dc06fa5
vadim3000 <vadimgolban3000@gmail.com>
2021-06-06 11:51:31 +0300

    the last updates

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Curriculum Vadim Golban</title>
+</head>
+<body>
+	<h1>Vadim Golban1</h1>
+	<p>Email: vadimgolban3000@gmail.com</p>
+
+	<div>
+        <?php echo "Today is " . date("Y/m/d H:i:s"); ?>
+    </div>
+</body>
+</html>
