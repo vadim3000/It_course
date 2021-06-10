aaf5ce42bffb169df524ece0f9e17ef5d67e6222
vadim3000 <vadimgolban3000@gmail.com>
2021-06-10 23:40:25 +0300

    second commit, added  header, main section, and footer, also block with video

--- public/index.php
+++ public/index.php
@@ -1,15 +1,42 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title>Curriculum Vadim Golban</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
-<body>
-	<!-- <h1>Vadim Golban1</h1>
-	<p>Email: vadimgolban3000@gmail.com</p> -->
+    <body>
+        <header>
+            <h1>Vadim Golban CV!</h1>    
+            <nav>
+                <ul>
+                    <li>About me</li>
+                    <li>Expirience</li>
+                    <li>Skills</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>  
+        </header>    
+        <!-- <p>Hello Pes</p> -->
 
-	<div>
+<section id="main">
+	
+<!-- Section video is not working for the moment -->
+     <!-- <section id="main_video">
+        <video controls="controls">
+            <source src="https://vimeo.com/560554785" type="video/mp4"> 
+        </video>
+    </section>  -->
+
+</section>
+
+<footer>
+<div>
         <?php echo "Today is " . date("Y/m/d H:i:s"); ?>
     </div>
-	<p>afsdfsdfsdfsdfds</p>
-</body>
+
+</footer>
+    </body>
 </html>
+
