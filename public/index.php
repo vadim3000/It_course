d81de4c94f611dbefb8cd61b9ce75f53bcfff6f1
vadim3000 <vadimgolban3000@gmail.com>
2021-06-11 00:01:24 +0300

    added Skills, and about me section

--- public/index.php
+++ public/index.php
@@ -11,17 +11,36 @@
             <h1>Vadim Golban CV!</h1>    
             <nav>
                 <ul>
-                    <li>About me</li>
+                    <li><a href="#about_me">About me</a></li>
+					<li><a href="#skills">Skills</a></li>
                     <li>Expirience</li>
-                    <li>Skills</li>
                     <li>Contact</li>
                 </ul>
             </nav>  
         </header>    
         <!-- <p>Hello Pes</p> -->
 
-<section id="main">
-	
+	<section id="main">
+			<section id="about_me">
+				<p>Hello My Name is Vadim, I'm working as QA Engineer, and I'm curious about IT and Web Developing. 
+				</p>
+
+				<img src="https://scontent.fkiv5-1.fna.fbcdn.net/v/t1.6435-9/47233402_1402396146569306_4894100583658553344_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=qzkQixfFXvMAX-3ny_K&_nc_ht=scontent.fkiv5-1.fna&oh=906d6aa3c205cb23817cf58b25095ee3&oe=60E7DC61" alt="Vadim_Golban_avatar" width="250px" height="250px">
+			</section>
+
+			<section id="skills">
+				<h3>My Skills</h3>
+				<ul>
+					<li>Html and CSS</li>
+					<li>Manual Testing</li>
+					<li>Automation Testing</li>
+					<li>Performance Testing</li>
+					<li>PHP</li>
+					<li>MySql</li>
+					<li>Laravel</li>
+				</ul>
+			</section>	
+					
 <!-- Section video is not working for the moment -->
      <!-- <section id="main_video">
         <video controls="controls">
