ab444e7bd937eec849ee7f6319ad67d78b5548e4
vadim3000 <vadimgolban3000@gmail.com>
2021-06-10 23:28:38 +0300

    fisrt commit added html branch

--- public/index.php
+++ public/index.php
@@ -4,11 +4,12 @@
 <title>Curriculum Vadim Golban</title>
 </head>
 <body>
-	<h1>Vadim Golban1</h1>
-	<p>Email: vadimgolban3000@gmail.com</p>
+	<!-- <h1>Vadim Golban1</h1>
+	<p>Email: vadimgolban3000@gmail.com</p> -->
 
 	<div>
         <?php echo "Today is " . date("Y/m/d H:i:s"); ?>
     </div>
+	<p>afsdfsdfsdfsdfds</p>
 </body>
 </html>
