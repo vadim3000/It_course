c1e72cbcebe24893de3366ac031b5e93a7783586
vadim3000 <vadimgolban3000@gmail.com>
2021-06-11 00:20:05 +0300

    3 commit  added Expirience and about me sections

--- public/index.php
+++ public/index.php
@@ -4,25 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Vadim Golban CV</title>
 </head>
     <body>
         <header>
-            <h1>Vadim Golban CV!</h1>    
+            <h1>Vadim Golban CV</h1>    
             <nav>
                 <ul>
                     <li><a href="#about_me">About me</a></li>
 					<li><a href="#skills">Skills</a></li>
-                    <li>Expirience</li>
-                    <li>Contact</li>
+                    <li><a href="#expirience">Expirience</a></li>
+                    <li><a href="#about_me">About me</a></li>
                 </ul>
             </nav>  
         </header>    
-        <!-- <p>Hello Pes</p> -->
 
 	<section id="main">
 			<section id="about_me">
-				<p>Hello My Name is Vadim, I'm working as QA Engineer, and I'm curious about IT and Web Developing. 
+				<p>
+					Hello My Name is Vadim, I'm working as QA Engineer, and I'm curious about IT and Web Developing. 
 				</p>
 
 				<img src="https://scontent.fkiv5-1.fna.fbcdn.net/v/t1.6435-9/47233402_1402396146569306_4894100583658553344_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=qzkQixfFXvMAX-3ny_K&_nc_ht=scontent.fkiv5-1.fna&oh=906d6aa3c205cb23817cf58b25095ee3&oe=60E7DC61" alt="Vadim_Golban_avatar" width="250px" height="250px">
@@ -40,7 +40,22 @@
 					<li>Laravel</li>
 				</ul>
 			</section>	
-					
+			
+			<section id="expirience">
+				<h3>My expirience</h3>
+				<p>10/2019 - now, QA Engineer at Picanova Group</p>
+				<p>02/2016 -07/2016, Internship at Optimum Web</p>
+			</section>		
+
+           <section id="about_me">
+		   <h2>Contact</h2>
+				<ul>
+					<li>Tel:<a href="tel:+1234567890">+1234567890</a></li>
+					<li>Email: vadimgolban3000@gmail.com</li>
+					<li>Address: city Chisinau Drumul Viilor str.</li>
+				</ul>
+		   </section>
+
 <!-- Section video is not working for the moment -->
      <!-- <section id="main_video">
         <video controls="controls">
@@ -48,14 +63,14 @@
         </video>
     </section>  -->
 
-</section>
+	</section>
 
-<footer>
-<div>
-        <?php echo "Today is " . date("Y/m/d H:i:s"); ?>
-    </div>
+		<footer>
+			<section id="date" align="center">
+				<?php echo "Today is " . date("Y/m/d H:i:s"); ?>
+			</section>
 
-</footer>
+		</footer>
     </body>
 </html>
 
