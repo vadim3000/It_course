d4d44511bb1131d5a1207fd8ee6cccebc3157df2
vadim3000 <vadimgolban3000@gmail.com>
2021-06-19 19:39:37 +0300

    added styles for lists and grid system for education section

--- public/index.php
+++ public/index.php
@@ -12,60 +12,73 @@
 		<!-- <link rel="stylesheet" href="assets/style.css"> -->
 </head>
     <body>
+	<div class="container">
         <header>
-            <h1>Vadim Golban CV</h1>    
-            <nav id="navigation">
-                <ul>
-                    <li><a href="#about_me">About me</a></li>
-					<li><a href="#education">Education</a></li>
-					<li><a href="#skills">Skills</a></li>
-                    <li><a href="#expirience">Expirience</a></li>
-					<li><a href="#my_work">My work</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                </ul>
-            </nav>  
+			<div class="row">
+				<h1>Vadim Golban CV</h1>    
+				<nav id="navigation">
+				<!-- <div class="container-fluid"> -->
+				<ul>
+						<li><a href="#about_me">About me</a></li>
+						<li><a href="#education">Education</a></li>
+						<li><a href="#skills">Skills</a></li>
+						<li><a href="#expirience">Expirience</a></li>
+						<li><a href="#my_work">My work</a></li>
+						<li><a href="#contact">Contact</a></li>
+					</ul>
+				</nav>
+			</div> 
         </header>    
 
 		<section id="main">
+			<div class="row">
 			<section id="about_me">
 				<p>
 					Hello My Name is Vadim, I'm working as QA Engineer, and I'm curious about IT and Web Developing. 
 				</p>
-			</section>
+			</div>	
+		</section>
             
-			<section id="education">
-				<div id="float_block-1">
-				<h2>My Education</h2>
-					<ul>
-						<li>State University of Moldova, Faculty of Economics, specialty  "Business & Management".</li>
-						<li>Master degree. Academy of Economic Studies of Moldova, specialty “Marketing Management”</li>
-						<li>"Endava Testing course”</li>
-					</ul>
+		<section id="education">
+			<div class="row">
+				<div class="col-sm-8">
+					<h2>My Education</h2>
+					<ul class="list-group-flush">
+							<li class="list-group-item">State University of Moldova, Faculty of Economics, specialty  "Business & Management".</li>
+							<li class="list-group-item">Master degree. Academy of Economic Studies of Moldova, specialty “Marketing Management”</li>
+							<li class="list-group-item">"Endava Testing course”</li>
+						</ul>
 				</div>
-				<div id="float_block-2">
-					<img id="avatar_img"src="assets/images/my_avatar_image.jpg" alt="Vadim_Golban_avatar_image" width="250px" height="250px">
+				<div class="col-sm-4">
+					<div class="card" style="width: 18rem;">
+						<img src="assets/images/my_avatar_image.jpg" class="card-img-top" alt="Vadim_Golban_avatar_image"">
+						<div class="card-body">
+							<p class="card-text">Vadim Golban QA Engineer</p>
+						</div>
+					</div>
 				</div>
-			</section>	
+			</div>
+		</section>	
+
 
 			<section id="skills">
 				<h2>My Skills</h2>
-				<ul>
-					<li>Html and CSS</li>
-					<li>Manual Testing</li>
-					<li>Automation Testing</li>
-					<li>Performance Testing</li>
-					<li>PHP</li>
-					<li>MySql</li>
-					<li>Laravel</li>
-					<li>Linux/Ubuntu_OS</li>
-					<li>Git</li>
+				<ul class="list-group-numbered">
+					<li class="list-group-item">Html and CSS</li>
+					<li class="list-group-item">Manual Testing</li>
+					<li class="list-group-item">Automation Testing</li>
+					<li class="list-group-item">Performance Testing</li>
+					<li class="list-group-item">PHP</li>
+					<li class="list-group-item">MySql</li>
+					<li class="list-group-item">Laravel</li>
+					<li class="list-group-item">Linux/Ubuntu_OS</li>
+					<li class="list-group-item">Git</li>
 				</ul>
 			</section>	
 		
 			<section id="expirience">
 				<h2>My expirience</h2>
 				<p>10/2019 - now, QA Engineer at Picanova Group</p>
-                <p>10/2018 - 04.2019, Endava Testing Course</p>
 				<p>02/2016 - 07/2016, Internship at Optimum Web</p>
 			</section>		
 
@@ -80,11 +93,11 @@
 			
 			<section id="contact">
 		         <h2>Contact</h2>
-				    <ul>
-                        <li><span class="contact-list">Tel:</span><a href="tel:+1234567890"> +12345690</a></li>
-                        <li><span class="contact-list">Email:</span> vadimgolban3000@gmail.com</li>
-                        <li><span class="contact-list">Address:</span> City Chisinau Drumul Viilor 111 str, apt 88</li>
-                        <li><span class="contact-list">Social network:</span><a href="https://www.linkedin.com/in/vadim-golban-27a61b156/"> Linkedin profile link<a></li>
+				 <ul class="list-group-flush">
+                        <li class="list-group-item"><span class="contact-list">Tel:</span><a href="tel:+1234567890"> +12345690</a></li>
+                        <li class="list-group-item"><span class="contact-list">Email:</span> vadimgolban3000@gmail.com</li>
+                        <li class="list-group-item"><span class="contact-list">Address:</span> City Chisinau Drumul Viilor 111 str, apt 88</li>
+                        <li class="list-group-item"><span class="contact-list">Social network:</span><a href="https://www.linkedin.com/in/vadim-golban-27a61b156/"> Linkedin profile link<a></li>
 				    </ul>
 		   </section>
 
@@ -104,6 +117,8 @@
                 </p>
 			</section>
 	</footer>
+	</div>
+
     </body>
 </html>
 
